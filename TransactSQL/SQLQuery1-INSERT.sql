aa537b6863246a91c93401df813644b794fa3219
supamodd <ghosts.zz@yandex.ru>
2026-01-18 12:38:22 +0300

    upd2

--- TransactSQL/SQLQuery1-INSERT.sql
+++ TransactSQL/SQLQuery1-INSERT.sql
@@ -1,51 +1,37 @@
 --SQLQuery1-INSERT.sql
 USE SPU_411_Import;
+SET NOCOUNT ON;
 
+DECLARE @group      INT     = (SELECT group_id     FROM Groups      WHERE group_name = N'SPU 411');
+DECLARE @discipline SMALLINT = (SELECT discipline_id FROM Disciplines WHERE discipline_name LIKE N'Процедурное%C++');
+DECLARE @teacher    INT     = (SELECT teacher_id   FROM Teachers    WHERE first_name LIKE N'Олег');
 
-DECLARE	@group		AS	INT			=	(SELECT group_id		FROM Groups			WHERE group_name=N'SPU 411');
-DECLARE	@discipline	AS	SMALLINT	=	(SELECT discipline_id	FROM Disciplines	WHERE discipline_name LIKE	(N'Процедурное%C++'));
-DECLARE	@number_of_lessons	AS	TINYINT	=	(SELECT number_of_lessons	FROM Disciplines	WHERE discipline_id = @discipline);
-DECLARE	@lesson_number	AS	INT	=	1;
-DECLARE	@teacher		AS	INT	=	(SELECT	teacher_id FROM Teachers	WHERE first_name LIKE(N'Олег'));
-DECLARE	@start_date		AS	DATE	=	N'2024-10-26';
-DECLARE	@date			AS	DATE	=	@start_date;
-DECLARE	@start_time		AS	TIME	=	N'10:00';
-DECLARE	@time			AS	TIME	=	@start_time
+DECLARE @start_date DATE = '20241026';
+DECLARE @end_date   DATE = '20260117';
+DECLARE @start_time TIME = '10:00:00';
 
-PRINT(@group)
-PRINT(@discipline)
-PRINT(@number_of_lessons)
-PRINT(@teacher)
-PRINT(@date)
-PRINT(@start_time);
+DECLARE @cur_date DATE = @start_date;
+DECLARE @inserted INT = 0;
 
-
-WHILE	(@lesson_number <= @number_of_lessons)
+WHILE @cur_date <= @end_date
 BEGIN
-		PRINT('-----------------------------');
-		PRINT (@date);
-		----------------------------------------------
-		SET @time=@start_time
-		INSERT Schedule
-		([group], discipline, teacher, [date], [time], spent)
-VALUES	(@group, @discipline, @teacher, @date, @time, IIF(@date<GETDATE(),1,0 ));
-		PRINT (@time);
-		SET @lesson_number=@lesson_number+1;
-		-----------------------------------------------
-		SET @time = DATEADD(MINUTE, 95, @time);
-		PRINT (@time);
-		INSERT Schedule
-		([group], discipline, teacher, [date], [time], spent)
-VALUES	(@group, @discipline, @teacher, @date, @time, IIF(@date<GETDATE(),1,0));
-		SET @lesson_number=@lesson_number+1;
-		------------------------------------------------
-		SET @time = DATEADD(MINUTE, 95, @time);
-		PRINT (@time);
-		INSERT Schedule
-		([group], discipline, teacher, [date], [time], spent)
-VALUES	(@group, @discipline, @teacher, @date, @time, IIF(@date<GETDATE(),1,0 ));
-		SET @lesson_number=@lesson_number+1;
-		--------------------------------------------------
-		SET @date = DATEADD(DAY, 7, @date);
+    DECLARE @t1 TIME = @start_time;
+    DECLARE @t2 TIME = DATEADD(MINUTE, 95, @t1);
+    DECLARE @t3 TIME = DATEADD(MINUTE, 95, @t2);
 
+    INSERT Schedule ([group], discipline, teacher, [date], [time], spent)
+    VALUES (@group, @discipline, @teacher, @cur_date, @t1, 1);
+
+    INSERT Schedule ([group], discipline, teacher, [date], [time], spent)
+    VALUES (@group, @discipline, @teacher, @cur_date, @t2, 1);
+
+    INSERT Schedule ([group], discipline, teacher, [date], [time], spent)
+    VALUES (@group, @discipline, @teacher, @cur_date, @t3, 1);
+
+    SET @inserted += 3;
+
+    SET @cur_date = DATEADD(DAY, 7, @cur_date);
 END
+
+PRINT 'Добавлено пар: ' + CAST(@inserted AS varchar(10));
+PRINT 'Последняя дата: ' + CONVERT(varchar(10), DATEADD(DAY, -7, @cur_date), 104);
