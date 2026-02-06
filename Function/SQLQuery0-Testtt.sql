11d804456870146ea780afc466018ae5549e0388
supamodd <ghosts.zz@yandex.ru>
2026-02-06 18:06:43 +0300

    upd6

--- Function/SQLQuery0-Testtt.sql
+++ Function/SQLQuery0-Testtt.sql
@@ -1,9 +1,9 @@
 --SQLQuery0-Testtt
-SET DATEFIRST 1;
+--SET DATEFIRST 1;
 USE SPU_411_Import;
 
---PRINT(dbo.GetLastDate(N'PV_319'));
---PRINT(dbo.GetNextDay(N'PD_212'));
---PRINT(dbo.GetNextDate(N'PD_212'));
---PRINT(dbo.GetStartDate(N'PD_212'));
---PRINT(dbo.GetNextDate(N'PV_319'));
+PRINT(dbo.GetLastDate(N'PD_212'));
+PRINT(dbo.GetNextDay(N'PD_212'));
+PRINT(dbo.GetNextDate(N'PD_212'));
+PRINT(dbo.GetStartDate(N'PD_212'));
+PRINT(dbo.GetNextDate(N'PD_212'));
